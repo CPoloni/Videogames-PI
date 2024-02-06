31effb569a88ec92e3b0d2bf0a5b23fbfe5fe9d7
Cintia Poloni <cintia.poloni@gmail.com>
2024-02-06 00:07:40 -0300

    06/2

--- api/src/controllers/getVideogames.js
+++ api/src/controllers/getVideogames.js
@@ -6,42 +6,34 @@ const { API_KEY } = process.env;
 
 //********** GET/VIDEOGAMES **********//
 
-/* ----- para obtener Videogames de la API ----- */
+/* ----- obtengo 100 Videogames de la API ----- */
 const vgApi = async () => {
-  let url = new URL("https://api.rawg.io/api/games");
-
-  url.searchParams.set("key", API_KEY);
-
   let allVideogames = []; // aca voy a guardar todos los videogames (api)
   try {
-    for (let i = 0; i < 5; i++) {
-      const respuesta = await axios.get(url.href); // puedo usar un for each
+    for (let i = 1; i < 5; i++) {
+      const respuesta = await axios.get(
+        `https://api.rawg.io/api/games?key=${API_KEY}&page${i}`
+      );
       respuesta.data.results.map((vg) => {
+        // puedo usar un for each
         allVideogames.push({
           id: vg.id,
           name: vg.name,
           image: vg.background_image,
           rating: vg.rating,
           //mapeo genres y platforms que tienen varios objetos dentro de un array
-
           genres: vg.genres.map((g) => g.name),
           platforms: vg.platforms.map((p) => p.platform.name),
         });
       });
-      url = new URL(respuesta.data.next); // cambia la url a la url siguiente
-      //console.log({ url });
     }
     return allVideogames;
   } catch (error) {
-    // if (axios.isAxiosError(error)) {
-    //   console.log(error.response.data);
-    // } else {
-    console.log(error);
-    // }
+    throw Error(error.message);
   }
 };
 
-/* ----- para obtener Videogames de la DB ----- */
+/* ----- obtengo todos los Videogames de la DB ----- */
 const vgDb = async () => {
   try {
     return await Videogame.findAll({
@@ -54,26 +46,88 @@ const vgDb = async () => {
       ],
     });
   } catch (error) {
-    console.log(error);
+    throw Error(error.message);
   }
 };
 /* ----- uno las dos informaciones de la DB y Api ----- */
 const vgTotal = async () => {
   const infoApi = await vgApi(); //guardo la info q me trae la ejecucin de vgApi
   const infoDb = await vgDb(); //guardo la info q me trae la ejecucin de vgDb
-  //concateno;
-  //const infoTotal = infoApi.concat(infoDb);
   const infoTotal = [...infoApi, ...infoDb];
   return infoTotal;
 };
-// exporto info total
+// exporto vgTotal
 
 //********** GET/VIDEOGAMES/NAMES?= **********//
+
+/* ----- obtengo Videogames por nombre de la API ----- */
+const vgByNameApi = async (name) => {
+  let videogamesName = []; // guardo los videogames que coincidan con el nombre (api)
+  try {
+    const respuesta = await axios.get(
+      `https://api.rawg.io/api/games?search=${name}&key=${API_KEY}`
+    );
+    respuesta.data.results.map((vg) => {
+      videogamesName.push({
+        id: vg.id,
+        name: vg.name,
+        image: vg.background_image,
+        rating: vg.rating,
+        genres: vg.genres.map((g) => g.name),
+        platforms: vg.platforms.map((p) => p.platform.name),
+      });
+    });
+    return videogamesName;
+  } catch (error) {
+    //throw Error(error.message);
+    if (axios.isAxiosError(error)) {
+      console.log(error.response.data);
+    } else {
+      console.log(error);
+    }
+  }
+};
+
+/* ----- obtengo Videogames por nombre de la DB----- */
+const vgByNameDb = async (name) => {
+  try {
+    return await Videogame.findAll({
+      where: {
+        name: name,
+      },
+      include: [
+        {
+          model: Genres, // de este model quiero q me incluya solo atributo name
+          attributes: ["name"],
+          through: { atributes: [] },
+        },
+      ],
+    });
+  } catch (error) {
+    throw Error(error.message);
+  }
+};
+
+/* ----- uno las dos informaciones de la DB y Api ----- */
+const totalByName = async () => {
+  const byNameApi = await vgByNameApi(); //guardo la info q me trae la ejecucin de vgByNameApi
+  const byNameDb = await vgByNameDb(); //guardo la info q me trae la ejecucin de vgByNameDb
+  const byNametotal = [...byNameApi, ...byNameDb];
+  if (byNametotal.length < 1) {
+    return "There are no results for that name";
+  } else if (byNametotal.length >= 1 && byNametotal.length <= 15) {
+    return byNametotal;
+  } else {
+    //return byNametotal.slice(0, 15);
+    const byName15 = byNametotal.slice(0, 15);
+    return byName15;
+  }
+};
 
 //SI HAY NAME TENGO QUE DEVOLVER LOS PRIMEROS 15 DE LA API Y BASE DE DATOS
 //con un metdo slice corto el array que me guarda con los nombres
 
 //********** GET/VIDEOGAMES/:IDVIDEOGAMES= **********//
 
-module.exports = { vgTotal };
+module.exports = { vgTotal, totalByName };
 //para juntar los videogames de bd y de la api: allVideogames  = [...bd, ...api]
--- api/src/routes/route_videogames.js
+++ api/src/routes/route_videogames.js
@@ -1,20 +1,20 @@
 const { Router } = require("express");
-const { vgTotal } = require("../controllers/getVideogames");
+const { vgTotal, totalByName } = require("../controllers/getVideogames");
 
 const router = Router();
 //recordar el next
-// en esta ruta voy a traer todos los vg (api y db)o si me pasan el name por query los primeros 15 resultados.
+// en esta ruta TRAIGO todos los vg (api y db) o si me pasan el name por query los primeros 15 resultados.
 router.get("/", async (req, res) => {
-  //let { name } = req.query;
-  let allVg = await vgTotal();
+  let { name } = req.query;
+  let resVideogames;
 
-  // CUANDO TENGA MI CONTROLLERS POR NAME PORNER EL IF(NAME), ELSE TODO
-  // if (name) {
-  //   try {
-  //   } catch (error) {}
-  // }
   try {
-    res.status(200).json(allVg);
+    if (name) {
+      resVideogames = await totalByName(name);
+    } else {
+      resVideogames = await vgTotal();
+    }
+    res.status(200).json({ resVideogames });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
